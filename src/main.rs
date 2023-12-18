4efff72011868287261396fccca3795fd11045f2
hedrickw <selfishtoaster@gmail.com>
2023-12-18 09:23:26 -0800

    ugly day 3 part 1 solution

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,11 @@
 mod day_1;
 mod day_2;
+mod day_3;
 pub use crate::day_1::solution::run;
 pub use crate::day_2::solution::run as run2;
+pub use crate::day_3::solution::run as run3;
 fn main() {
     // run();
-    run2();
+    // run2();
+    run3();
 }
