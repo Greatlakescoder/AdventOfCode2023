4c4d0efba6144203cd4d737aa486e36e93b3cb6a
hedrickw <selfishtoaster@gmail.com>
2023-12-22 15:53:29 -0800

    super slow but working version of day 4 part 2

--- src/day_4/solution.rs
+++ src/day_4/solution.rs
@@ -1,6 +1,6 @@
+use std::collections::{HashMap, HashSet};
 use std::fs::File;
 use std::io::{BufRead, BufReader};
-use std::collections::HashSet;
 
 fn read_file(path: &str) -> Result<i32, std::io::Error> {
     // Read file line by line
@@ -9,6 +9,7 @@ fn read_file(path: &str) -> Result<i32, std::io::Error> {
 
     // let mut parts: Vec<&str> = Vec::new();
     let mut total = 0;
+    let mut card_mapping: HashMap<u32, u32> = HashMap::new();
     for line in reader.lines() {
         match line {
             Ok(line) => {
@@ -17,31 +18,77 @@ fn read_file(path: &str) -> Result<i32, std::io::Error> {
                 let my_numbers: Vec<&str> = card_parts[1].trim().split(" ").collect();
                 let unique_numbers: HashSet<&str> = HashSet::from_iter(my_numbers);
                 let winning_numbers = card_parts[0].trim().split(" ").collect::<Vec<&str>>();
-                let mut output_numbers = Vec::new();
-                let mut score: i32 = 0;
-                for number in winning_numbers {
-                    if number == "" {
-                        continue;
+                let mut score: u32 = 0;
+                let card_number: u32 = card[0].split(" ").collect::<Vec<&str>>()[1]
+                    .trim()
+                    .parse::<u32>()
+                    .unwrap();
+                if card_mapping.contains_key(&card_number) {
+                  
+                    score = *card_mapping.get(&card_number).unwrap();
+                    card_mapping.insert(card_number, score + 1);
+                    for _i in 0..score {
+                        fun_name(
+                            winning_numbers.clone(),
+                            unique_numbers.clone(),
+                            &mut card_mapping,
+                            card_number + 1,
+                            score,
+                        );
                     }
-                    if unique_numbers.contains(&number) {
-                        output_numbers.push(number);
-                        if score == 0 {
-                            score = 1;
-                        }else {
-                            score *= 2;
-                        }
-                    }
+                    fun_name(
+                        winning_numbers.clone(),
+                        unique_numbers.clone(),
+                        &mut card_mapping,
+                        card_number + 1,
+                        score,
+                    );
+                } else {
+                    card_mapping.insert(card_number, 1);
+                    fun_name(
+                        winning_numbers,
+                        unique_numbers,
+                        &mut card_mapping,
+                        card_number + 1,
+                        score,
+                    );
                 }
-                println!("{} = {}", output_numbers.join(" "), score);
-                output_numbers.clear();
-                total += score
             }
             Err(e) => {
                 println!("Error: {}", e);
             }
         }
     }
+    for (key, value) in card_mapping.iter() {
+      
+            total += *value as i32;
+        
+    }
+    println!("{:?}", card_mapping);
     return Ok(total);
+}
+
+fn fun_name(
+    winning_numbers: Vec<&str>,
+    unique_numbers: HashSet<&str>,
+    card_mapping: &mut HashMap<u32, u32>,
+    mut card_number: u32,
+    mut score: u32,
+) {
+    for number in winning_numbers {
+        if number == "" {
+            continue;
+        }
+        if unique_numbers.contains(&number) {
+            if card_mapping.contains_key(&card_number) {
+                score = *card_mapping.get(&card_number).unwrap();
+                card_mapping.insert(card_number, score + 1);
+            } else {
+                card_mapping.insert(card_number, 1);
+            }
+            card_number += 1;
+        }
+    }
 }
 pub fn run() {
     match read_file("src/day_4/input.txt") {
